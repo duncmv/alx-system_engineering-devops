47dfbe29d5aa8ed0e6935fd0f61a4ea2dceba187
duncmv <dtindiwensi@gmail.com>
2024-02-23 15:31:43 +0300

    tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,22 @@
 # install flask using pip3
+package { 'python3-pip':
+  ensure => installed,
+}
+package { 'setuptools':
+  ensure   => installed,
+  provider => 'pip3',
+}
+
+package { 'wheel':
+  ensure   => installed,
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '1.0.1',
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',

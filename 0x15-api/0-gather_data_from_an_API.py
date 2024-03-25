820ff6ddd6f3e35359c135d1f3d0339fa7dba49f
duncmv <dtindiwensi@gmail.com>
2024-03-25 12:08:46 +0300

    tasks

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,15 +7,15 @@ import sys
 
 if __name__ == "__main__":
     id = int(sys.argv[1])
-
-    with requests.get(f"https://jsonplaceholder.typicode.com/users/{id}") as resp:
+    url = f"https://jsonplaceholder.typicode.com/users/{id}"
+    with requests.get(url) as resp:
         name = resp.json().get('name')
 
     url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
     with requests.get(url) as resp:
         todos = resp.json()
 
-    completed = [todo for todo in todos if todo.get('completed') is True]
-    print(f"Employee {name} is done with tasks({len(completed)}/{len(todos)}):")
-    for todo in completed:
+    comp = [todo for todo in todos if todo.get('completed') is True]
+    print(f"Employee {name} is done with tasks({len(comp)}/{len(todos)}):")
+    for todo in comp:
         print(f"\t {todo.get('title')}")

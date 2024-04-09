20b89035962c68874f229a622c9a28eef0b12aa2
duncmv <dtindiwensi@gmail.com>
2024-04-09 17:40:08 +0300

    mandatory

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""gets the to ten subreddit posts"""
+import requests
+
+
+def top_ten(subreddit):
+    """ GET subreddit top 10 hot posts """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'user-agent': 'duncan'}
+    r = requests.get(url, headers=headers)
+    if (r.status_code is 404):
+        print("None")
+    elif 'data' not in r.json():
+        print("None")
+    else:
+        r = r.json()
+        for post in r['data']['children']:
+            print(post['data']['title'])
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+more API requests
